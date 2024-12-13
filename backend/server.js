521a390d7c146186c323f3a5a06e14548620f53c
unknown <meseretaddisu10@gmail.com>
2024-12-13 03:24:55 -0800

    first commit

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+
+
+const express = require("express");
+// console.log("Express module loaded");
+
+const app = express();
+// console.log("Express app initialized");
+
+// app.get("/", (req, res) => {
+//     console.log("GET request received at '/'");
+//     res.send("Server is running!");
+// });
+
+app.listen(8081, () => {
+    console.log("Server is listening on port 8081");
+    // process.exit(1)
+});
