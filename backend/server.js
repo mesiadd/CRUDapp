13962e92c04c25cf07a3067720a682dc275e66bf
unknown <meseretaddisu10@gmail.com>
2024-12-13 03:35:23 -0800

    serverjs updated

--- backend/server.js
+++ backend/server.js
@@ -6,10 +6,9 @@ const express = require("express");
 const app = express();
 // console.log("Express app initialized");
 
-// app.get("/", (req, res) => {
-//     console.log("GET request received at '/'");
-//     res.send("Server is running!");
-// });
+ app.get("/", (req, res) => {     console.log("GET request received at '/'");
+    res.send("Server is running!");
+ });
 
 app.listen(8081, () => {
     console.log("Server is listening on port 8081");
